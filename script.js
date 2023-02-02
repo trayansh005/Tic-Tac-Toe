cbe868edf6f57d7ab526f6c10d2f5ccddde2f332
trayansh <74540362+trayansh005@users.noreply.github.com>
2023-02-02 12:22:16 +0530

    added draw logic

--- script.js
+++ script.js
@@ -1,40 +1,55 @@
+let startGame = true;
 const container = document.querySelector(".container");
 const boxes = container.querySelectorAll(".box");
 const winnerText = document.querySelector(".winner");
 const playRoundBtn = document.querySelector(".play");
 
-let choices = [];
-let startGame = true;
+let choice = [];
 
 const gameBoard = (() => {
 	const createBoard = () => {
-		choices = Array(9).fill("");
+		for (let i = 0; i < 9; i++) {
+			choice.push("");
+		}
 	};
 
 	const playRound = (player1, player2) => {
-		let player1Turn = true;
+		let player1turn = true;
 
-		boxes.forEach((box, i) => {
+		for (let i = 0; i < boxes.length; i++) {
+			const box = boxes[i];
 			box.addEventListener("click", () => {
-				if (!startGame || box.innerHTML !== "") return;
-
-				let player = player1Turn ? player1 : player2;
-				box.innerHTML = player.marker;
-				player1Turn = !player1Turn;
-				choices[i] = player.marker;
-				checkWinner(player1, player2);
+				if (startGame && player1turn === true && box.innerHTML === "") {
+					box.innerHTML = player1.marker;
+					player1turn = false;
+					let index = box.getAttribute("data-index");
+					choice[index] = player1.marker;
+					console.log(choice);
+					checkWinner(player1, player2);
+				} else if (startGame && player1turn === false && box.innerHTML === "") {
+					box.innerHTML = player2.marker;
+					player1turn = true;
+					let index = box.getAttribute("data-index");
+					choice[index] = player2.marker;
+					console.log(choice);
+					checkWinner(player1, player2);
+				}
 			});
-		});
+		}
 	};
 
 	return { createBoard, playRound };
 })();
 
-const createPlayer = (name, marker) => ({
-	name,
-	marker,
-	sayName: () => console.log(`Player ${name}`),
-});
+function createPlayer(name, marker) {
+	return {
+		name: name,
+		marker: marker,
+		sayName() {
+			console.log(`Player ${name}`);
+		},
+	};
+}
 
 const player = createPlayer("Player", "X");
 const cpu = createPlayer("CPU", "O");
@@ -50,25 +65,50 @@ const winningCombinations = [
 	[2, 4, 6],
 ];
 
-const checkWinner = (player1, player2) => {
-	for (let [a, b, c] of winningCombinations) {
-		if (choices[a] === player1.marker && choices[b] === player1.marker && choices[c] === player1.marker) {
-			winnerText.textContent = `${player1.name} wins!`;
+const checkWinner = (player, cpu) => {
+	for (let i = 0; i < winningCombinations.length; i++) {
+		const [a, b, c] = winningCombinations[i];
+		if (choice[a] === player.marker && choice[b] === player.marker && choice[c] === player.marker) {
+			console.log(`Player ${player.name} wins!`);
+			winnerText.textContent = `${player.name} wins!`;
 			startGame = false;
 			playRoundBtn.style.display = "block";
 			return;
-		} else if (choices[a] === player2.marker && choices[b] === player2.marker && choices[c] === player2.marker) {
-			winnerText.textContent = `${player2.name} wins!`;
+		} else if (choice[a] === cpu.marker && choice[b] === cpu.marker && choice[c] === cpu.marker) {
+			console.log(`Player ${cpu.name} wins!`);
+			winnerText.textContent = `${cpu.name} wins!`;
 			startGame = false;
 			playRoundBtn.style.display = "block";
 			return;
+		} else if (!checkWinner) {
+			winnerText.textContent = `DRAW`;
+			startGame = false;
+			playRoundBtn.style.display = "block";
 		}
+	}
+	checkDraw();
+};
+
+const checkDraw = () => {
+	let draw = true;
+	for (let i = 0; i < choice.length; i++) {
+		if (choice[i] === "") {
+			draw = false;
+			break;
+		}
+	}
+	if (draw) {
+		startGame = false;
+		playRoundBtn.style.display = "block";
+		winnerText.textContent = "It's a draw!";
 	}
 };
 
 const restartGame = () => {
-	choices = [];
-	boxes.forEach((box) => (box.innerHTML = ""));
+	choice = [];
+	for (let i = 0; i < boxes.length; i++) {
+		boxes[i].innerHTML = "";
+	}
 	startGame = true;
 	winnerText.textContent = "Tic-Tac-Toe";
 	gameBoard.createBoard();
